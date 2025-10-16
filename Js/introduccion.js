c73554c64faf5e6ac19b93db3443def703dbe21a
Diego <dipzon03@gmail.com>
2025-10-15 19:25:28 -0600

    Introduccion a JavaScript

---
+++ Js/introduccion.js
@@ -0,0 +1,119 @@
+// Operadores
+// 2 + 2 // 4 Suma
+// 4 - 2 // 2 Resta
+// 3 * 2 // 6 Multiplicación
+// 2 / 2 // 1 Divisón
+// 2 % 2 // 0 Módulo (Devuelve el residuo de la división)
+// 3 ** 3 // 27 Potencia
+
+// Jerarquia de operaciones
+// 2 + 2 * 3 = 8;
+// (2 + 2) * 3 = 12;
+
+// Strings y/o cadenas de texto
+
+// 'Estás aprendiendo JS' // 'Strings, tuplas'
+// "Estás aprendiendo JS" // "Strings, tuplas"
+// `Estás aprendiendo JS` // `Son solamente tuplas` console.log()
+
+// Template string 
+
+// `Poderosisimas nalgas de tu hermana`
+// `Poderosisimas nalgas de ${}` 
+// function saludar(Nombre,Edad){
+//     console.log(`Hola, ${Nombre}, tienes ${Edad} años?`)
+// }
+
+// Concatenación
+
+// "Yo soy " + "Diego Pérez Muñoz"
+
+// Operadores de comparación
+
+// 5 > 3 // true
+// 5 < 3 // false
+// 5 >= 3 // true
+// 5 >= 5 // true
+// 5 <= 3 // false
+// 5 <= 5 // true
+
+// 5 === 5 // true (compara valor y tipo)
+// 5 !== 5 // false (Se está preguntando en negativo)
+
+// 'JavaScript' === 'JavaScript' // True
+// 'JavaScript' === 'Java' // False
+// 'JavaScript' !== 'PHP' // True 
+// `Estoy Aprendiendo JavaScript` === 'Estoy Aprendiendo JavaScript'
+// '10' === 10
+// `10` === '10'
+
+// 'a' > 'A' // True
+// 'A' > 'B' // False (Código ASCII)
+
+// Comparadores lógicos
+
+// True && False
+// 2 < 3 && 3 < 4 // True
+// 2 + 2 < 3 && 10 < 8 * 2 
+// // Primero se hacen las operaciones aritméticas:
+// // → 4 < 3 && 10 < 16
+// // Ahora las comparaciones:
+// // → false && true
+// // Finalmente:
+// // → false
+
+// true && true && true // True
+// (true && true) || false // false
+// !true && !true // false
+// (false && true) || !true // false
+
+// Redondeo 
+
+// Para abajo utilizamos Math.floor() 
+// Para arriba utilizamos Math.ceil() 
+
+
+// Variables 
+// // Para declarar variables puedes utilizar var (global), let (local) y const (constante)(global o local).
+
+// let numero
+// let numero1 = 1
+
+// Constantes 
+
+// const PI = 3.1416
+// PI = 3 // TypeError 
+
+// const radio // Error de sintaxis, siempre se deben inicializar con algun valor
+
+// let variable1 = 1
+// let variable2 = 2
+
+// variable1 + variable2
+
+// Nomenclaturas 
+
+// camelCase // Para nombrar variables y funciones
+// snake_case // Para nombrar variables y funciones
+// SCREAMING_CASE // Se suele utilizar par nombrar constantes
+
+// /* Comentarios de varias lineas */
+// // Comentarios de una sola línea
+
+// // console.error(): Imprime un mensaje de error en la consola.
+// // console.warn(): Imprime un mensaje de advertencia en la consola.
+// // console.info(): Imprime un mensaje de información en la consola.
+
+// null y undefined
+
+// Parecen lo mismo pero no lo sessionStorage.  
+
+// null solamente puede tener null como valor
+// undefined solamente puede tener undefinde como valor
+
+// let papelDeBaño // undefined
+// let papelDeBaño = null 
+
+// null != 0 // true
+// null == 0 // false
+
