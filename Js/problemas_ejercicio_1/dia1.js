fca716263d6344960798611dcf741dc26cd7bd96
Diego <dipzon03@gmail.com>
2025-10-16 17:29:43 -0600

    Ejercicios de JS

--- Js/problemas_ejercicio_1/dia1.js
+++ Js/problemas_ejercicio_1/dia1.js
@@ -1,3 +1,8 @@
 // Día 1
 
+// Ejercicio 1: Dado a, b, c; encuentra el mayor de los 3 números
+
+// Ejercicio 2: Pide a tus compañeros dos números y un operador y muestra el resultado (Pueba en consola del navegador)
+
+// Ejercicio 3: Dado un número del 1 al 100, indica si el rango es: 1 - 25 -> Bajo; 26 - 50 -> medio; 51 - 75 -> Alto; 76 - 100 -> Muy alto
 
