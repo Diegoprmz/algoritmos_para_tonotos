329be20cbd50abcc159e27af533b19d896511752
Raul <raulitopv06@gmail.com>
2025-10-16 18:43:38 -0600

    Ejercio 1_raul

---
+++ Js/problemas_ejercicio_1/dia1 copy_raul.js
@@ -0,0 +1,14 @@
+// Día 1
+
+// Ejercicio 1: Dado a, b, c; encuentra el mayor de los 3 números
+
+function numeroMayor(a,b,c){
+    a = 3
+    b = 5
+    c = 1
+}
+
+// Ejercicio 2: Pide a tus compañeros dos números y un operador y muestra el resultado (Pueba en consola del navegador)
+
+// Ejercicio 3: Dado un número del 1 al 100, indica si el rango es: 1 - 25 -> Bajo; 26 - 50 -> medio; 51 - 75 -> Alto; 76 - 100 -> Muy alto
+
