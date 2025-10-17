d5eaa7fc388902739dae5ccf24567e3e735f9ec0
unknown <jeshydmedina@gmail.com>
2025-10-16 18:40:08 -0600

    Intento de actividad

---
+++ Js/problemas_ejercicio_1/dia1CopyJeshyd.js
@@ -0,0 +1,34 @@
+// Día 1
+
+// Ejercicio 1: Dado a, b, c; encuentra el mayor de los 3 números
+/*function numeroMayor(a,b,c){
+    if(a>=b && a>=c){
+        console.log(`${a} es mayor`)
+    }else if(b>=a && b>=c){
+        console.log(`${b} es mayor`)
+    }else{
+        console.log(`${c} es mayor`)
+    }
+}
+numeroMayor(4,5,6)
+*/
+// Ejercicio 2: Pide a tus compañeros dos números y un operador y muestra el resultado (Pueba en consola del navegador)
+/*function resultado(a,b,operador){
+//alch se me trabo el cerebro
+}
+*/
+// Ejercicio 3: Dado un número del 1 al 100, indica si el rango es: 1 - 25 -> Bajo; 26 - 50 -> medio; 51 - 75 -> Alto; 76 - 100 -> Muy alto
+/*function rango(numero){
+    if(numero>=76 && numero<=100) {
+        console.log("Muy alto")
+    }else if(numero>=51 && numero<76){
+        console.log("Alto")
+    }else if(numero>=26 && numero<51){
+        console.log("medio")
+    }else if(numero>=1 && numero<26){
+        console.log("bajo")
+    }
+}
+
+rango(50)
+*/
